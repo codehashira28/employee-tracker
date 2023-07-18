2661dc1e4acbd7ccf0200933708715c99ca4d632
codehashira28 <123044813+codehashira28@users.noreply.github.com>
2023-07-17 20:55:41 -0500

    finished adding code for a fully functioning application

--- index.js
+++ index.js
@@ -33,6 +33,54 @@ const viewAllEmployees = (tableName) => {
 
 }
 
+const getRoles = () => {
+    let roles = [];
+    db.query('SELECT title FROM roles', (err, data) => {
+        if(err) console.error(err);
+        else data.forEach(role => roles.push(role.title));
+    });
+    return roles;
+}
+
+const getManagers = () => {
+    let managers = [];
+    db.query('SELECT CONCAT(first_name, " ", last_name) as manager FROM employees', (err, data) => {
+        if(err) console.error(err);
+        else return data.forEach(person => managers.push(person.manager));
+    })
+    return managers;
+}
+
+const getDepartments = () => {
+    let departments = [];
+    db.query('SELECT name FROM departments', (err, data) => {
+        if(err) console.error(err);
+        else return data.forEach(department => departments.push(department.name));
+    })
+    return departments;
+}
+
+const setRoleId = (role, callback) => {
+    db.query(`SELECT id FROM roles WHERE title = '${role}'`, (err, data) => {
+        if(err) console.error(err);
+        else callback(err, data[0].id);
+    })  
+}
+
+const setMangerId = (manager, callback) => {
+    db.query(`SELECT role_id FROM employees WHERE CONCAT(first_name, " ", last_name) = '${manager}'`, (err, data) => {
+        if(err) console.error(err);
+        else callback(err, data[0].role_id);
+    })  
+}
+
+const setDepartmentId = (deptName, callback) => {
+    db.query(`SELECT id FROM departments WHERE name = '${deptName}'`, (err, data) => {;
+    if(err) console.error(err)
+    else callback(err, data[0].id);
+    })
+}
+
 const viewAllRoles = (tableName) => {
     db.query(`SELECT roles.id, title, name AS department, salary
     FROM roles
@@ -73,6 +121,104 @@ const handleAction = (choice) => {
                 })
             })
             break;
+        
+        case 'Add Employee':
+            prompt([
+                {
+                    name: 'first_name',
+                    message: "What is the employee's first name?"
+                },
+                {
+                    name: 'last_name',
+                    message: "What is the employee's last name?"
+                },
+                {
+                    type: 'list',
+                    name: 'role',
+                    message: "What is the employee's role?",
+                    choices: getRoles()
+                },
+                {
+                    type: 'list',
+                    name: 'manager',
+                    message: "Who is the employee's manager?",
+                    choices: getManagers()
+                }
+            ]).then((answers) => {
+                db.query(`INSERT INTO employees (first_name, last_name) VALUES ('${answers.first_name}', '${answers.last_name}')`);
+                setRoleId(answers.role, function(err, data) {
+                    if(err) console.error(err);
+                    db.query(`UPDATE employees SET role_id = (${data}) WHERE first_name = '${answers.first_name}' AND last_name = '${answers.last_name}'`);
+                });
+                setMangerId(answers.manager, function(err, data) {
+                    if(err) console.error(err);
+                    db.query(`UPDATE employees SET manager_id = (${data}) WHERE first_name = '${answers.first_name}' AND last_name = '${answers.last_name}'`);
+                });
+                init();
+            })
+            break;
+
+        case 'Add Role':
+            prompt([
+                {
+                    name: 'newrole',
+                    message: 'What is the name of the role?'
+                },
+                {
+                    name: 'salary',
+                    message: 'What is the salary of the role?'
+                },
+                {
+                    type: 'list',
+                    name: 'department',
+                    message: 'Which department does it belong to?',
+                    choices: getDepartments()
+                }
+            ]).then((answers) => {
+                db.query(`INSERT INTO roles (title, salary) VALUES ('${answers.newrole}', ${answers.salary})`);
+                setDepartmentId(answers.department, function(err, data) {
+                    if(err) console.error(err);
+                    db.query(`UPDATE roles SET department_id = (${data}) WHERE title = '${answers.newrole}'`);
+                })
+                init();
+            })
+            break;
+
+        case 'Update Employee Role':
+            db.query('SELECT CONCAT(first_name, " ", last_name) as employee FROM employees', (err, data) => {
+                let employees = [];
+                data.forEach((obj) => {
+                    employees.push(obj.employee);
+                })
+                db.query('SELECT title FROM roles', (err, roledata) => {
+                    let roles = [];
+                    roledata.forEach((rolobj) => {
+                        roles.push(rolobj.title);
+                    })
+                prompt([
+                    {
+                        type: 'list',
+                        name: 'employee',
+                        message: "Which employee's role do you want to update?",
+                        choices: employees
+                    },
+                    {
+                        type: 'list',
+                        name: 'newrole',
+                        message: "Which role do you want to assign the selected employee?",
+                        choices: roles
+                    }
+                ])
+                .then((answers) => {
+                    setRoleId(answers.newrole, function(err, data) {
+                        if(err) console.error(err);
+                        db.query(`UPDATE employees SET role_id = (${data}) WHERE CONCAT(first_name, " ", last_name) = '${answers.employee}'`);
+                    });
+                    init();
+                })
+            })
+        })
+        break;
 
         default: {
             process.exit();
@@ -83,7 +229,7 @@ const handleAction = (choice) => {
 
 const init = () => {
     prompt({
-        type: 'rawlist',
+        type: 'list',
         name: 'action',
         message: 'What would you like to do?',
         choices: [
@@ -102,4 +248,3 @@ const init = () => {
 }
 
 init();
-
